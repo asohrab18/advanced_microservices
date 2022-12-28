955625bb4735ec3f04a7cebe92294820eb7373f3
Sohrab Alam <sohrab.alam86@gmail.com>
2022-12-28 16:36:28 +0530

    6. Some more @Value annotation tricks

--- spring-boot-config/src/main/java/com/configurations/springbootconfig/controller/AppController.java
+++ spring-boot-config/src/main/java/com/configurations/springbootconfig/controller/AppController.java
@@ -1,5 +1,8 @@
 package com.configurations.springbootconfig.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,22 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default value bcoz property does not exist in its file.}")
 	private String greetingMessage;
-	
+
 	@Value("${app.name}")
 	private String applicationName;
-	
+
 	@Value("${app.description}")
 	private String applicationDesc;
 
+	@Value("Some static message")
+	private String staticMessage;
+
+	@Value("${my.list.values}")
+	private List<String> listValues;
+
+	@Value("#{${dbValues}}")
+	private Map<String, String> dbValues;
+
 	@GetMapping("/greetings")
 	public String greetUser() {
-		return greetingMessage;
+		return greetingMessage + "<br/>" + staticMessage + "<br/>" + listValues+ "<br/>" + dbValues;
 	}
-	
+
 	@GetMapping("/app-details")
 	public String showAppDetails() {
-		return "Application Name: "+applicationName+"<br/>Description: "+applicationDesc;
+		return "<b>Application Name:</b> " + applicationName + "<br/><b>Description:</b> " + applicationDesc;
 	}
 }
