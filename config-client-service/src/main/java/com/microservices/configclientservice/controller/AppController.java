a6b613490530fc11f3d2a7eab09e8926b7678347
Sohrab Alam <sohrab.alam86@gmail.com>
2023-01-12 14:27:36 +0530

    Config Client fetched properties from Config Server's GIT Repo

--- config-client-service/src/main/java/com/microservices/configclientservice/controller/AppController.java
+++ config-client-service/src/main/java/com/microservices/configclientservice/controller/AppController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.configclientservice.DbSettings;
 
 @RestController
+@RefreshScope
 public class AppController {
 
 	@Value("${my.greeting: default value bcoz property does not exist in its file.}")
